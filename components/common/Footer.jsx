20d54f4beb7c24e553894227ad5626575f655474
saman keighobadi <saman.keighobadi@gmail.com>
2022-01-24 00:19:58 +0330

    feat: react icons
    install react icons

--- components/common/Footer.jsx
+++ components/common/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h1>Footer </h1>
+            {/* <h1>Footer </h1> */}
         </div>
     );
 }
--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <div>
-            navbar
+  return (
+    <div className="">
+      <nav className="bg-blue-600 h-14 sticky top-0 flex items-center justify-between px-5">
+        <ul className="flex   text-white ">
+          <li className="py-1 px-2">Home</li>
+          <li className="py-1 px-2">Products</li>
+          <li className="py-1 px-2">About </li>
+          <li className="py-1 px-2">Contact </li>
+        </ul>
+        <div className="relative text-center">
+          <span>card icon</span>
+          <div className="absolute top-0 left-0 w-5 h-5 bg-blue-800 rounded-3xl shadow-lg grid place-content-center">
+            <span className=" text-white rounded-full text-sm ">2</span>
+          </div>
         </div>
-    );
-}
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
