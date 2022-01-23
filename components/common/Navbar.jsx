d41d2526f8a495a68ff1c176ad0683efb8261d15
saman keighobadi <saman.keighobadi@gmail.com>
2022-01-24 00:29:08 +0330

    style: navbar layout
    style and design navbar

--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {FiShoppingCart} from 'react-icons/fi'
 
 const Navbar = () => {
   return (
@@ -10,10 +11,10 @@ const Navbar = () => {
           <li className="py-1 px-2">About </li>
           <li className="py-1 px-2">Contact </li>
         </ul>
-        <div className="relative text-center">
-          <span>card icon</span>
-          <div className="absolute top-0 left-0 w-5 h-5 bg-blue-800 rounded-3xl shadow-lg grid place-content-center">
-            <span className=" text-white rounded-full text-sm ">2</span>
+        <div className="relative text-center w-10 h-10  grid place-content-center">
+          <div className="text-3xl text-white    "><FiShoppingCart /></div>
+          <div className="absolute top-0 right-0 w-5 h-5 text-xs bg-blue-800 rounded-3xl shadow-lg grid place-content-center">
+            <span className=" text-white rounded-full  ">2</span>
           </div>
         </div>
       </nav>
