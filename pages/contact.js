023b225409c88ef37b58cb705156f423df8b237a
saman keighobadi <saman.keighobadi@gmail.com>
2022-01-24 23:56:58 +0330

    refactor: contact page
    complete contact page & structure

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div>
-            <h1>contanct us</h1>
+  return (
+    <div className="grid place-content-center py-9">
+      <div className=" max-w-3xl">
+        <h1 className="text-5xl pb-4 font-sebold text-gray-700">Contact us</h1>
+        <div className="pt-6 text-gray-500">
+          <p className="font-semibold text-justify">
+            Reprehenderit adipisicing reprehenderit non aliquip quis enim elit
+            consequat et amet pariatur pariatur proident quis. Ad enim anim amet
+            id aliquip ex eiusmod irure exercitation qui adipisicing minim est
+            culpa. Exercitation qui cupidatat magna culpa aute sint.
+          </p>
         </div>
-    );
-}
+        <div className="py-10 text-gray-500">
+          <p className="font-semibold text-justify">
+            Reprehenderit adipisicing reprehenderit non aliquip quis enim elit
+            consequat et amet pariatur pariatur proident quis. Ad enim anim amet
+            id aliquip ex eiusmod irure exercitation qui adipisicing minim est
+            culpa. Exercitation qui cupidatat magna culpa aute sint.
+          </p>
+        </div>
+        <div className="pt-3 text-gray-500">
+          <p className="font-semibold text-justify">
+            Reprehenderit adipisicing reprehenderit non aliquip quis enim elit
+            consequat et amet pariatur pariatur proident quis. Ad enim anim amet
+            id aliquip ex eiusmod irure exercitation qui adipisicing minim est
+            culpa. Exercitation qui cupidatat magna culpa aute sint.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
