fdbaf46a7ae721bab57df9fe0a641d97a1006b5b
saman keighobadi <saman.keighobadi@gmail.com>
2022-02-01 23:42:05 +0330

    style: add images to public folder

--- components/hero/Slider.jsx
+++ components/hero/Slider.jsx
@@ -21,9 +21,22 @@ SwiperCore.use([Navigation]);
 
 const Slider = () => {
 
+    const sliders = [
+        {id:1,src:'https://images.samsung.com/nl/smartphones/galaxy-s21/buy/02_ImageCarousel/03_GroupShot-T2/S21plus_Group_KV_MO_img.jpg'},
+        {id:2,src:'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Capture_7_8.JPG'
+    },
+        {id:3,src:'https://tvadsongs.uk/wp-content/uploads/2017/09/Samsung-Galaxy-Note8-Discover-the-Note8-TV-Ad.png'
+    },
+    ]
+
     return (
         <div>
             <Swiper navigation={true} autoplay={true} className='h-96'>
+            {sliders.map((slider,index) => (
+                <SwiperSlide key={index}>
+                    <Image alt='saman' src={slider.src} width={100} height={100} />
+                </SwiperSlide>
+            ))}
                 <SwiperSlide className='bg-red-600'>
                  1
                 </SwiperSlide>
