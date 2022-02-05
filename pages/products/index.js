8e459c8d89d2030ce1c6e11adf85b75f832aaf9a
saman keighobadi <saman.keighobadi@gmail.com>
2022-02-05 17:01:14 +0330

    chore: install prettier plugin for tailwindcss
    format tailwindcss classes by prettier plugin

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -12,14 +13,23 @@ const Products = () => {
   ];
 
   return (
-    <div className="pt-3 px-12">
+    <div className="pt-3 px-12  font-sans">
       <div className="grid gap-4  lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 grid-cols-1  place-content-center">
         {productList.map((product, index) => (
-          <div key={index} className=" max-w-md shadow-lg  rounded-lg bg-red-50">
+          <div
+            key={index}
+            className=" font-sans font-normal text-gray-700 text-justify px-6 max-w-md shadow-lg  rounded-lg bg-red-50 "
+          >
+          <Image alt=''  width={100} height={100} src={'/images/img3.png'} />
             <h4>{product.name}</h4>
+            <p className="">
+              Quis elit exercitation fugiat laboris cillum irure consequat
+              fugiat id dolore laborum occaecat ad elit. 
+            </p>
             <Link href={"/product/"} as={`/products/${product.id}`} passHref>
-              <button className="px-2 py-1 bg-blue-500 text-gray-50 font-medium rounded-lg">Buy now</button>
-                    
+              <button className="px-2 py-1 my-2 bg-blue-500 text-gray-50 font-medium rounded-lg">
+                Buy now
+              </button>
             </Link>
           </div>
         ))}
