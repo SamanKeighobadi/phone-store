27ea657d3c71bd2db56b8fe0c699f0022cf273e7
saman keighobadi <saman.keighobadi@gmail.com>
2022-02-13 22:45:31 +0330

    feat: create cart page
    cart page for see products

---
+++ pages/cart.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div>
+            <div>
+                <table>
+                    <thead>
+                        <th>Product</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Total</th>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default Cart;
