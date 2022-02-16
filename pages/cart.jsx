b7e8ea561ce81203c7dccd32e44166684ee7ce07
saman keighobadi <saman.keighobadi@gmail.com>
2022-02-16 21:40:51 +0330

    style: total cart

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const Cart = () => {
   return (
     <div className="container mt-6  h-96   ">
-      <div>
-        <table className="text-zinc-700 shadow-lg mx-5">
+      <div className="flex  ">
+        <table className="mx-5 text-zinc-700 shadow-lg">
           <thead className="text-center">
             <th className="px-6">Product</th>
             <th className="px-6">Name</th>
@@ -14,7 +14,7 @@ const Cart = () => {
             <th className="px-6">Total</th>
           </thead>
           <tbody>
-            <tr className="text-center pb-5">
+            <tr className="pb-5 text-center">
               <td>
                 <Image
                   src={"/images/iphonE13pro.png"}
@@ -26,13 +26,19 @@ const Cart = () => {
               </td>
               <td>IPhone 13 pro</td>
               <td>$999</td>
-              <td>
-                1
-              </td>
+              <td>1</td>
               <td>$999</td>
             </tr>
           </tbody>
         </table>
+        <div className="flex flex-col justify-between capitalize text-zinc-700">
+          <h5 className="text-lg font-semibold">commodity prices: $89</h5>
+          <h5 className="text-lg font-semibold">shipping: $10</h5>
+          <h5 className="text-lg font-semibold">total: $99</h5>
+          <button className="duration-0 hover:duration-150 rounded-md bg-sky-600 py-2  text-white transition hover:bg-sky-700">
+            Purches
+          </button>
+        </div>
       </div>
     </div>
   );
