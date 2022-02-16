d6adac53a9b727e0c4335a4d11d126b282e87852
saman keighobadi <saman.keighobadi@gmail.com>
2022-02-16 21:27:23 +0330

    style: change table of product layout

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -3,8 +3,37 @@ import Image from "next/image";
 
 const Cart = () => {
   return (
-    <div className="container mt-6  h-96 grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1  ">
-    
+    <div className="container mt-6  h-96   ">
+      <div>
+        <table className="text-zinc-700 shadow-lg mx-5">
+          <thead className="text-center">
+            <th className="px-6">Product</th>
+            <th className="px-6">Name</th>
+            <th className="px-6">Price</th>
+            <th className="px-6">Quantity</th>
+            <th className="px-6">Total</th>
+          </thead>
+          <tbody>
+            <tr className="text-center pb-5">
+              <td>
+                <Image
+                  src={"/images/iphonE13pro.png"}
+                  alt=""
+                  width={150}
+                  height={150}
+                  layout="fixed"
+                />
+              </td>
+              <td>IPhone 13 pro</td>
+              <td>$999</td>
+              <td>
+                1
+              </td>
+              <td>$999</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
